bbd1ae5bbfb1f473802df3f5aaa83fde45ba6fc3
Satsuki Akiba <anosatsuk124@gmail.com>
2022-06-21 18:23:06 +0000

    fix

--- src/args.rs
+++ src/args.rs
@@ -7,11 +7,14 @@ pub struct Args {
     pub help: Option<bool>,
 }
 
-pub fn args_parse() -> Result<Args, ()>{
+pub fn args_parse() -> Result<Args, ()> {
     let args: Vec<String> = std::env::args().collect();
 
-    match args.len() {
-        2 => Ok(Args{filepath: Some(OsString::from(&args[1])), help: None}),
+    match args.len() - 1 {
+        1 => Ok(Args {
+            filepath: Some(OsString::from(&args[1])),
+            help: None,
+        }),
         _ => Err(()),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 mod args;
-use std::{io::{stdin, stdout, Write}, env, fs, path, cmp::{min, max}};
+use std::{
+    cmp::{max, min},
+    fs,
+    io::{stdin, stdout, Write},
+    path,
+};
 
-use termion::{event::{Event, Key}, raw::IntoRawMode, input::TermRead, clear, cursor, screen::AlternateScreen};
+use termion::{
+    clear, cursor,
+    event::{Event, Key},
+    input::TermRead,
+    raw::IntoRawMode,
+    screen::AlternateScreen,
+};
 
 #[derive(Eq, PartialEq, Debug)]
 struct Cursor {
@@ -19,25 +30,22 @@ struct Editor {
 impl Default for Editor {
     fn default() -> Self {
         Self {
-            buf: vec!(Vec::new()),
-            cursor: Cursor{
-                row: 0, column: 0,
-            },
+            buf: vec![Vec::new()],
+            cursor: Cursor { row: 0, column: 0 },
             row_offset: 0,
         }
-   } 
+    }
 }
 
 impl Editor {
     fn open(&mut self, path: &path::Path) {
-        self.buf = fs::read_to_string(path).ok()
-            .map(|s| {s.lines()
-            .map(|line| line.chars().collect()).collect()}).unwrap();
+        self.buf = fs::read_to_string(path)
+            .ok()
+            .map(|s| s.lines().map(|line| line.chars().collect()).collect())
+            .unwrap();
 
-        self.cursor = Cursor {
-            row: 0,
-            column: 0,
-        }
+        self.cursor = Cursor { row: 0, column: 0 };
+        self.row_offset = 0;
     }
 
     fn terminal_size() -> (usize, usize) {
@@ -48,83 +56,72 @@ impl Editor {
     fn draw<T: Write>(&self, out: &mut T) {
         let (rows, cols) = Self::terminal_size();
 
-        // Clear screen
         write!(out, "{}", clear::All).unwrap();
-
-        // Set the cursor left-top (1-indexed)
         write!(out, "{}", cursor::Goto(1, 1)).unwrap();
 
         let mut row = 0;
         let mut col = 0;
 
-        let mut cursor: Option<Cursor> = None;
+        let mut display_cursor: Option<(usize, usize)> = None;
 
         'outer: for i in self.row_offset..self.buf.len() {
             for j in 0..=self.buf[i].len() {
                 if self.cursor == (Cursor { row: i, column: j }) {
-                    cursor = Some(Cursor {
-                        row,
-                        column: col,
-                    })
+                    display_cursor = Some((row, col));
                 }
 
-            if let Some(c) = self.buf[i].get(j) {
-                write!(out, "{}", c).unwrap();
-                col += 1;
-                if col >= cols{
-                    row += 1;
-                    col = 0;
-                    if row >= rows {
-                        break 'outer;
-                    } else {
-                        write!(out, "\r\n").unwrap();
+                if let Some(c) = self.buf[i].get(j) {
+                    write!(out, "{}", c).unwrap();
+                    col += 1;
+                    if col >= cols {
+                        row += 1;
+                        col = 0;
+                        if row >= rows {
+                            break 'outer;
+                        } else {
+                            write!(out, "\r\n").unwrap();
+                        }
                     }
                 }
             }
-            }
             row += 1;
             col = 0;
-
-            if row > rows {
+            if row >= rows {
                 break;
             } else {
                 write!(out, "\r\n").unwrap();
             }
         }
 
-        if let Some(cursor) = cursor {
-            write!(
-                out,
-                "{}",
-                cursor::Goto(cursor.column as u16 + 1, cursor.row as u16 + 1)
-            ).unwrap();
+        if let Some((r, c)) = display_cursor {
+            write!(out, "{}", cursor::Goto(c as u16 + 1, r as u16 + 1)).unwrap();
         }
 
         out.flush().unwrap();
     }
-
     fn scroll(&mut self) {
         let (rows, _) = Self::terminal_size();
         self.row_offset = min(self.row_offset, self.cursor.row);
         if self.cursor.row + 1 >= rows {
-            self.row_offset = max(self.row_offset, self.cursor.row + 1 - rows)
+            self.row_offset = max(self.row_offset, self.cursor.row + 1 - rows);
         }
     }
-
     fn cursor_up(&mut self) {
         if self.cursor.row > 0 {
             self.cursor.row -= 1;
             self.cursor.column = min(self.buf[self.cursor.row].len(), self.cursor.column);
         }
+        self.scroll();
     }
     fn cursor_down(&mut self) {
         if self.cursor.row + 1 < self.buf.len() {
             self.cursor.row += 1;
             self.cursor.column = min(self.cursor.column, self.buf[self.cursor.row].len());
         }
+        self.scroll();
     }
     fn cursor_left(&mut self) {
-        if self.cursor.column > 1 {
+        if self.cursor.column > 0 {
             self.cursor.column -= 1;
         }
     }
