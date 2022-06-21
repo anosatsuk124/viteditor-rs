349655272eab173791a6bd238bd6982c3b14222c
Satsuki Akiba <anosatsuk124@gmail.com>
2022-06-21 15:27:54 +0000

    implement scroll method

---
+++ src/args.rs
@@ -0,0 +1,17 @@
+use std::ffi::OsString;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Args {
+    pub filepath: Option<OsString>,
+    pub help: Option<bool>,
+}
+
+pub fn args_parse() -> Result<Args, ()>{
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => Ok(Args{filepath: Some(OsString::from(&args[1])), help: None}),
+        _ => Err(()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,171 @@
-use std::{io::{stdin, stdout, Write}, env, fs};
+mod args;
+use std::{io::{stdin, stdout, Write}, env, fs, path, cmp::{min, max}};
 
 use termion::{event::{Event, Key}, raw::IntoRawMode, input::TermRead, clear, cursor, screen::AlternateScreen};
 
+#[derive(Eq, PartialEq, Debug)]
+struct Cursor {
+    row: usize,
+    column: usize,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+struct Editor {
+    buf: Vec<Vec<char>>,
+    cursor: Cursor,
+    row_offset: usize,
+}
+
+impl Default for Editor {
+    fn default() -> Self {
+        Self {
+            buf: vec!(Vec::new()),
+            cursor: Cursor{
+                row: 0, column: 0,
+            },
+            row_offset: 0,
+        }
+   } 
+}
+
+impl Editor {
+    fn open(&mut self, path: &path::Path) {
+        self.buf = fs::read_to_string(path).ok()
+            .map(|s| {s.lines()
+            .map(|line| line.chars().collect()).collect()}).unwrap();
+
+        self.cursor = Cursor {
+            row: 0,
+            column: 0,
+        }
+    }
+
+    fn terminal_size() -> (usize, usize) {
+        let (cols, rows) = termion::terminal_size().unwrap();
+        (rows as usize, cols as usize)
+    }
+
+    fn draw<T: Write>(&self, out: &mut T) {
+        let (rows, cols) = Self::terminal_size();
+
+        // Clear screen
+        write!(out, "{}", clear::All).unwrap();
+
+        // Set the cursor left-top (1-indexed)
+        write!(out, "{}", cursor::Goto(1, 1)).unwrap();
+
+        let mut row = 0;
+        let mut col = 0;
+
+        let mut cursor: Option<Cursor> = None;
+
+        'outer: for i in self.row_offset..self.buf.len() {
+            for j in 0..=self.buf[i].len() {
+                if self.cursor == (Cursor { row: i, column: j }) {
+                    cursor = Some(Cursor {
+                        row,
+                        column: col,
+                    })
+                }
+
+            if let Some(c) = self.buf[i].get(j) {
+                write!(out, "{}", c).unwrap();
+                col += 1;
+                if col >= cols{
+                    row += 1;
+                    col = 0;
+                    if row >= rows {
+                        break 'outer;
+                    } else {
+                        write!(out, "\r\n").unwrap();
+                    }
+                }
+            }
+            }
+            row += 1;
+            col = 0;
+
+            if row > rows {
+                break;
+            } else {
+                write!(out, "\r\n").unwrap();
+            }
+        }
+
+        if let Some(cursor) = cursor {
+            write!(
+                out,
+                "{}",
+                cursor::Goto(cursor.column as u16 + 1, cursor.row as u16 + 1)
+            ).unwrap();
+        }
+
+        out.flush().unwrap();
+    }
+
+    fn scroll(&mut self) {
+        let (rows, _) = Self::terminal_size();
+        self.row_offset = min(self.row_offset, self.cursor.row);
+        if self.cursor.row + 1 >= rows {
+            self.row_offset = max(self.row_offset, self.cursor.row + 1 - rows)
+        }
+    }
+
+    fn cursor_up(&mut self) {
+        if self.cursor.row > 0 {
+            self.cursor.row -= 1;
+            self.cursor.column = min(self.buf[self.cursor.row].len(), self.cursor.column);
+        }
+    }
+    fn cursor_down(&mut self) {
+        if self.cursor.row + 1 < self.buf.len() {
+            self.cursor.row += 1;
+            self.cursor.column = min(self.cursor.column, self.buf[self.cursor.row].len());
+        }
+    }
+    fn cursor_left(&mut self) {
+        if self.cursor.column > 1 {
+            self.cursor.column -= 1;
+        }
+    }
+    fn cursor_right(&mut self) {
+        self.cursor.column = min(self.cursor.column + 1, self.buf[self.cursor.row].len());
+    }
+}
+
 fn main() {
+    let mut state = Editor::default();
+
     let stdin = stdin();
     let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
 
     // Read from the file
-    let buf: Vec<Vec<char>> = match args_parse().unwrap().filepath {
-        Some(p) => fs::read_to_string(p).ok()
-            .map(|s| {s.lines()
-            .map(|line| line.chars().collect()).collect()}).unwrap(),
+    match crate::args::args_parse().unwrap().filepath {
+        Some(p) => state.open(path::Path::new(&p)),
         _ => panic!("not exists"),
     };
 
-    // Clear screen
-    write!(stdout, "{}", clear::All).unwrap();
-
-    // Set the cursor 1-indexed
-    write!(stdout, "{}", cursor::Goto(1, 1)).unwrap();
-
-    for line in &buf {
-        for &c in line {
-            write!(stdout, "{}", c).unwrap();
-        }
-        write!(stdout, "\r\n").unwrap();
-    }
-
-    stdout.flush().unwrap();
+    state.draw(&mut stdout);
 
     for event in stdin.events() {
-        if event.unwrap() == Event::Key(Key::Ctrl('c')) {
-            return;
+        match event.unwrap() {
+            Event::Key(Key::Ctrl('c')) => {
+                return;
+            }
+            Event::Key(Key::Up) => {
+                state.cursor_up();
+            }
+            Event::Key(Key::Down) => {
+                state.cursor_down();
+            }
+            Event::Key(Key::Left) => {
+                state.cursor_left();
+            }
+            Event::Key(Key::Right) => {
+                state.cursor_right();
+            }
+            _ => {}
         }
-    }
-}
-
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Args {
-    filepath: Option<String>,
-    help: Option<bool>,
-}
-
-fn args_parse() -> Result<Args, ()>{
-    let args: Vec<String> = env::args().collect();
-
-    match args.len() {
-        2 => Ok(Args{filepath: Some(args[1].clone()), help: None}),
-        _ => Err(()),
+        state.draw(&mut stdout);
     }
 }
